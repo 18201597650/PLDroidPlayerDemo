9dcbc2d6044a53c2a35ec1fdb041df92d4149f79
18201597650 <18201597650@163.com>
2016-12-11 23:49:40 +0800

    再次修改

--- app/src/main/java/com/pili/pldroid/playerdemo/MainActivity.java
+++ app/src/main/java/com/pili/pldroid/playerdemo/MainActivity.java
@@ -4,6 +4,7 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.EditText;
@@ -39,6 +40,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Log.i("MainActivity","onCreate");
         setContentView(R.layout.activity_main);
 
         try {
